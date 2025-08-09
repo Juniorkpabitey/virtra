f64700cd0ecd743acc4a7e5c09db0831faa60914
Juniorkpabitey <juniorkpabitey123@gmail.com>
2025-08-09 22:04:44 +0000

    package update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "frwtypuoghhirvuwqyvj.supabase.co",
+        pathname: "/storage/v1/object/public/doctor-images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
